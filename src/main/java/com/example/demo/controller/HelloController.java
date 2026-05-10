b61f7fadb080df6167ef4b2a57582e0476e094b1
vinayak <vinayakprajapati246@gmail.com>
2026-05-10 17:24:58 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello(){
+        return "Spring Boot Application is running";
+    }
+    
+}
